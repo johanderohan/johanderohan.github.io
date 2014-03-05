4123625570c1902961c949c6c75890bbd0a17b65
Johan D. Rohan <johanderohan@gmail.com>
2014-03-05 17:35:24 +0100

    Basic structure

---
+++ js/default.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    //tolltip init
+    $("#tooltip1").tooltip({title: 'Who?'});
+    $("#tooltip2").tooltip({title: 'Skills'});
+    $("#tooltip3").tooltip({title: 'Work'});
+    $("#tooltip4").tooltip({title: 'Proyects'});
+    $("#tooltip5").tooltip({title: 'Social'});
+    $("#tooltip6").tooltip({title: 'Contact'});
+
+});
