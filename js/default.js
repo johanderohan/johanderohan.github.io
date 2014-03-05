215169e813c3ea69e52b0d314485d2e4ca1534c1
Johan D. Rohan <johanderohan@gmail.com>
2014-03-05 17:55:34 +0100

    Only show tooltip on Desktop browsers

--- js/default.js
+++ js/default.js
@@ -1,11 +1,25 @@
 $( document ).ready(function() {
 
-    //tolltip init
-    $("#tooltip1").tooltip({title: 'Who?'});
-    $("#tooltip2").tooltip({title: 'Skills'});
-    $("#tooltip3").tooltip({title: 'Work'});
-    $("#tooltip4").tooltip({title: 'Proyects'});
-    $("#tooltip5").tooltip({title: 'Social'});
-    $("#tooltip6").tooltip({title: 'Contact'});
+	if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+        || navigator.userAgent.match(/Opera Mini/i)
+        || navigator.userAgent.match(/IEMobile/i)
+        ) {/*es un móvil*/}
+        else{
+		    //tolltip init
+		    $("#tooltip1").tooltip({title: 'Who?'});
+		    $("#tooltip2").tooltip({title: 'Skills'});
+		    $("#tooltip3").tooltip({title: 'Work'});
+		    $("#tooltip4").tooltip({title: 'Proyects'});
+		    $("#tooltip5").tooltip({title: 'Social'});
+		    $("#tooltip6").tooltip({title: 'Contact'});
+		}
 
 });
+
+
